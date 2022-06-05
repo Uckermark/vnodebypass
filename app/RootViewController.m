366ba1d2fc7f5eb2aa1a8be012f5ccf5d5d77670
level3tjg <t.grantham14@gmail.com>
2022-06-04 23:11:53 -0400

    app dark mode

--- app/RootViewController.m
+++ app/RootViewController.m
@@ -14,30 +14,42 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
 
-  _titleLabel =
-      [[UILabel alloc] initWithFrame:CGRectMake(0, 50, UIScreen.mainScreen.bounds.size.width, 100)];
+  self.view.backgroundColor = UIColor.whiteColor;
+
+  _titleLabel = [[UILabel alloc]
+      initWithFrame:CGRectMake(0, 50, UIScreen.mainScreen.bounds.size.width,
+                               100)];
   _titleLabel.text = @"vnodebypass";
   _titleLabel.textAlignment = NSTextAlignmentCenter;
-  _titleLabel.textColor = UIColor.whiteColor;
+  _titleLabel.textColor = UIColor.blackColor;
   _titleLabel.font = [UIFont systemFontOfSize:40];
   [self.view addSubview:_titleLabel];
 
   _subtitleLabel = [[UILabel alloc]
-      initWithFrame:CGRectMake(0, 100, UIScreen.mainScreen.bounds.size.width, 100)];
+      initWithFrame:CGRectMake(0, 100, UIScreen.mainScreen.bounds.size.width,
+                               100)];
   _subtitleLabel.text = @"USE IT AT YOUR OWN RISK!";
   _subtitleLabel.textAlignment = NSTextAlignmentCenter;
-  _subtitleLabel.textColor = UIColor.whiteColor;
+  _subtitleLabel.textColor = UIColor.blackColor;
   _subtitleLabel.font = [UIFont systemFontOfSize:20];
   [self.view addSubview:_subtitleLabel];
 
   _button = [UIButton buttonWithType:UIButtonTypeSystem];
-  _button.frame = CGRectMake(UIScreen.mainScreen.bounds.size.width / 2 - 30,
-                             UIScreen.mainScreen.bounds.size.height / 2 - 25, 60, 50);
+  _button.frame =
+      CGRectMake(UIScreen.mainScreen.bounds.size.width / 2 - 30,
+                 UIScreen.mainScreen.bounds.size.height / 2 - 25, 60, 50);
   [_button setTitle:access("/bin/bash", F_OK) == 0 ? @"Enable" : @"Disable"
            forState:UIControlStateNormal];
   [_button addTarget:self
                 action:@selector(buttonPressed:)
       forControlEvents:UIControlEventTouchUpInside];
+
+  if (@available(iOS 13, *)) {
+    self.view.backgroundColor = UIColor.systemBackgroundColor;
+    _titleLabel.textColor = UIColor.labelColor;
+    _subtitleLabel.textColor = UIColor.labelColor;
+  }
+
   [self.view addSubview:_button];
 }
 
@@ -50,21 +62,24 @@
     opts = @[ @"-r", @"-R" ];
   }
 
-  NSString *launchPath =
-      [NSString stringWithFormat:@"/usr/bin/%@", NSProcessInfo.processInfo.processName];
-  NSTask *task = [NSTask launchedTaskWithLaunchPath:launchPath arguments:@[ opts[0] ]];
+  NSString *launchPath = [NSString
+      stringWithFormat:@"/usr/bin/%@", NSProcessInfo.processInfo.processName];
+  NSTask *task =
+      [NSTask launchedTaskWithLaunchPath:launchPath arguments:@[ opts[0] ]];
   [task waitUntilExit];
   task = [NSTask launchedTaskWithLaunchPath:launchPath arguments:@[ opts[1] ]];
   [task waitUntilExit];
   NSString *title = access("/bin/bash", F_OK) == 0 ? @"Enable" : @"Disable";
-  NSString *successTitle = (access("/bin/bash", F_OK) == 0) == disabled ? @"Failed" : @"Success";
+  NSString *successTitle =
+      (access("/bin/bash", F_OK) == 0) == disabled ? @"Failed" : @"Success";
   [_button setTitle:successTitle forState:UIControlStateNormal];
-  dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-    sleep(1);
-    dispatch_async(dispatch_get_main_queue(), ^{
-      [_button setTitle:title forState:UIControlStateNormal];
-    });
-  });
+  dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0),
+                 ^{
+                   sleep(1);
+                   dispatch_async(dispatch_get_main_queue(), ^{
+                     [_button setTitle:title forState:UIControlStateNormal];
+                   });
+                 });
 }
 
 @end
